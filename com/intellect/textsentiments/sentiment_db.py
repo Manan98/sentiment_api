ccaf124f3479ba72f409ab287ac8ac1d82fa7b15
Manan98 <manan.t.chugh@gmail.com>
2017-07-17 10:42:00 -0400

    For new results(July 24 file)

--- com/intellect/textsentiments/sentiment_db.py
+++ com/intellect/textsentiments/sentiment_db.py
@@ -4,16 +4,15 @@ from flask import Flask, request, Blueprint
 
 sentiment_db_api = Blueprint("sentiment_db_api", __name__)
 
-
 @sentiment_db_api.route('/api/sentiment', methods=['POST'])
 def get_articles():
     host = "mongodb://localhost:27017"
     print("############# initializing Mongo DB Client")
     client = MongoClient(host)
-    db = client.infomedia_db
-    sentiment_collection = db.infomedia_articles
+    db = client.infomedia_db_1
+    sentiment_collection = db.infomedia_articles_jul_14
     id = (str)(request.get_json()['ArticleId'])
     pt = (str)(request.get_json()['ProcessedTerm'])
-    sentiment_cursor = sentiment_collection.find({"ArticleId": id})
-    score = [field['avg_sentiment'][pt] for field in sentiment_cursor]
-    return json.dumps({"Score":score[0]})
+    sentiment_cursor = sentiment_collection.find({ "article_id": id })
+    score = [field['predicted_score'] for field in sentiment_cursor if pt.lower() in (str)(field['processed_terms']).lower()]
+    return json.dumps({"Score":score})
