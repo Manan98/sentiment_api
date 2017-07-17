8236197e6ab43301bd24ae85b78cf995bea81cec
Manan98 <manan.t.chugh@gmail.com>
2017-07-16 22:38:20 -0400

    Prospective overview and security description.

--- com/intellect/textsentiments/sentiment_db.py
+++ com/intellect/textsentiments/sentiment_db.py
@@ -2,17 +2,17 @@ import json
 from pymongo import MongoClient
 from flask import Flask, request, Blueprint
 
-sentiment_service_api = Blueprint("sentiment_service_api", __name__)
+sentiment_db_api = Blueprint("sentiment_db_api", __name__)
 
-@sentiment_service_api.route('/api/sentiment', methods=['POST'])
+@sentiment_db_api.route('/api/sentiment', methods=['POST'])
 def get_articles():
     host = "mongodb://localhost:27017"
     print("############# initializing Mongo DB Client")
     client = MongoClient(host)
     db = client.infomedia_db
     sentiment_collection = db.infomedia_articles
-    a = (str)(request.get_json()['ArticleId'])
-    b = (str)(request.get_json()['ProcessedTerm'])
-    sentiment_cursor = sentiment_collection.find({"ArticleId": a})
-    d = [field['avg_sentiment_1st_processed_term'][b] for field in sentiment_cursor]
-    return json.dumps({"Score":d[0]})
+    id = (str)(request.get_json()['ArticleId'])
+    pt = (str)(request.get_json()['ProcessedTerm'])
+    sentiment_cursor = sentiment_collection.find({"ArticleId": id})
+    score = [field['avg_sentiment'][pt] for field in sentiment_cursor]
+    return json.dumps({"Score":score[0]})
--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, send_from_directory
 import os
-from com.intellect.textsentiments.sentiment_db import sentiment_service_api
+from com.intellect.textsentiments.sentiment_db import sentiment_db_api
+#from com.intellect.textsentiments.sentiment_service import sentiment_service_api
 from flask.ext.cors import CORS
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-app.register_blueprint(sentiment_service_api)
+app.register_blueprint(sentiment_db_api)
+#app.register_blueprint(sentiment_service_api)
 
 @app.route('/api-docs/<path:path>')
 def launch_swagger(path):
